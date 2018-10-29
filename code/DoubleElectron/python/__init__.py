95dadeb355da6db933b2934bd9db6514ee178583
Diyaselis Delgado Lopez <didelgad@lxplus068.cern.ch>
2018-10-29 09:14:31 +0100

    code reorganization

---
+++ code/DoubleElectron/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/WorkDir/2011-minimumbias-compare-rereco/',1)[0])+'/cfipython/slc6_amd64_gcc472/WorkDir/2011-minimumbias-compare-rereco')
