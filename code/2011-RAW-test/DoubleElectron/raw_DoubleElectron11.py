f3e7309db104603ee504b3afd9def76af0018e1a
Diyaselis Delgado Lopez <didelgad@lxplus078.cern.ch>
2018-12-05 09:45:51 +0100

    Update code for 2011 samples - Missing lumi corrections

---
+++ code/2011-RAW-test/DoubleElectron/raw_DoubleElectron11.py
@@ -0,0 +1,74 @@
+#Auto generated configuration file
+# using: 
+# Revision: 1.381.2.28 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: reco -s RAW2DIGI,L1Reco,RECO,ALCA:@MinimumBias,USER:EventFilter/HcalRawToDigi/hcallaserhbhehffilter2012_cff.hcallLaser2012Filter,DQM:@MinimumBias --data --conditions FT_R_53_LV5::All --eventcontent RECO,AOD,DQM --datatier RECO,AOD,DQM --customise Configuration/DataProcessing/RecoTLR.customisePrompt --no_exec --python reco_2011A_MinimumBias.py
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('Configuration.StandardSequences.AlCaRecoStreams_cff')
+process.load('EventFilter.HcalRawToDigi.hcallaserhbhehffilter2012_cff')
+process.load('DQMOffline.Configuration.DQMOffline_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleElectron/RAW/v1/000/160/431/080D15C9-FF4D-E011-9484-0030487C7828.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.28 $'),
+    annotation = cms.untracked.string('reco nevts:1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.AODoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODEventContent.outputCommands,
+    fileName = cms.untracked.string('reco_DoubleElectron1111111111111111111111_AOD.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('AOD')
+    )
+)
+
+# Additional statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'FT_R_53_LV5::All', '')
+process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db')
+process.GlobalTag.globaltag = 'FT_53_LV5_AN1::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.user_step = cms.Path(process.hcallLaser2012Filter)
+process.AODoutput_step = cms.EndPath(process.AODoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.AODoutput_step)
+
---
+++ code/2011-RAW-test/SingleMu/analyzer_SingleMu11.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.categories.append('Demo')
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(
+        limit = cms.untracked.int32(-1)
+        )
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True), 
+										SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20000) )
+
+process.source = cms.Source("PoolSource",
+                 fileNames = cms.untracked.vstring('file:reco_SingleMu11_AOD.root'),
+                 lumisToProcess = cms.untracked.VLuminosityBlockRange('163289:1-163370:max')
+                #firstRun = cms.untracked.uint32(169957),     
+                #firstEvent = cms.untracked.uint32(488034889)
+                           )
+
+process.demo = cms.EDAnalyzer('PhysicsObjectsHistos',
+                minTracks=cms.untracked.uint32(0)
+                             )
+
+process.TFileService = cms.Service("TFileService",
+                       fileName = cms.string('histo_SingleMu11_RAW.root')
+                                  )
+
+process.p = cms.Path(process.demo)
+
